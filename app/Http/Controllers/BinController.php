fe03eae3e3ae6645237f06537175d93269912d9e
Pedro Perafan <pedro.perafan.carrasco@gmail.com>
2024-07-10 23:22:33 -0600

    Create bin classes

---
+++ app/Http/Controllers/BinController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBinRequest;
+use App\Http\Requests\UpdateBinRequest;
+use App\Models\Bin;
+
+class BinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBinRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Bin $bin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Bin $bin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBinRequest $request, Bin $bin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Bin $bin)
+    {
+        //
+    }
+}
